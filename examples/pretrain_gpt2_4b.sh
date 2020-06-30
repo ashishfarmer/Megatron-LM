23bd2f4a1db8dd91b1472dbb643f29c4e9f15e9e
ashish <ashish.farmer@amd.com>
2020-06-30 15:58:46 -0700

    add scripts for 4 configs

---
+++ examples/pretrain_gpt2_4b.sh
@@ -0,0 +1,53 @@
+#! /bin/bash
+
+# Runs the "4.2bn" parameter model
+
+GPUS_PER_NODE=$1
+MP=$2
+# Change for multinode config
+MASTER_ADDR=localhost
+MASTER_PORT=6000
+NNODES=1
+NODE_RANK=0
+WORLD_SIZE=$(($GPUS_PER_NODE*$NNODES))
+
+DATA_PATH=/dataset/wikipedia_merged_output_json_gpt2_text_document
+CHECKPOINT_PATH=checkpoints/gpt2
+
+DISTRIBUTED_ARGS="--nproc_per_node $GPUS_PER_NODE --nnodes $NNODES --node_rank $NODE_RANK --master_addr $MASTER_ADDR --master_port $MASTER_PORT"
+
+python3.6 -m torch.distributed.launch $DISTRIBUTED_ARGS \
+       pretrain_gpt2.py \
+       --model-parallel-size $MP \
+       --num-layers 64 \
+       --hidden-size 2304 \
+       --num-attention-heads 24 \
+       --batch-size 8 \
+       --seq-length 1024 \
+       --max-position-embeddings 1024 \
+       --train-iters 500 \
+       --lr-decay-iters 320000 \
+       --save $CHECKPOINT_PATH \
+       --load $CHECKPOINT_PATH \
+       --data-path $DATA_PATH \
+       --vocab-file /dataset/gpt2-vocab.json \
+       --merge-file /dataset/gpt2-merges.txt \
+       --data-impl mmap \
+       --split 949,50,1 \
+       --distributed-backend nccl \
+       --lr 0.00015 \
+       --lr-decay-style cosine \
+       --min-lr 1.0e-5 \
+       --weight-decay 1e-2 \
+       --clip-grad 1.0 \
+       --warmup .01 \
+       --checkpoint-activations \
+       --log-interval 100 \
+       --save-interval 10000 \
+       --eval-interval 1000 \
+       --eval-iters 10 \
+       --fp16
+
+
+
+set +x
